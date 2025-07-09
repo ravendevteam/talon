7b8b53f77edf8abffa960055790005680d814792
cringdahl <cory@coryringdahl.com>
2025-07-09 11:31:14 -0500

    update chrome package name

--- screens/screen_browser_select.py
+++ screens/screen_browser_select.py
@@ -56,11 +56,11 @@ def main():
     img_label = UIImage("browser_selection.png", parent=overlay)
     img_label.lower()
     browser_options = [
-        ("Chrome",              "googlechrome",   (251, 191, 14)),
-        ("Edge",                "microsoft-edge", (44, 195, 193)),
-        ("Brave (Recommended)", "brave",          (255, 47, 0)),
-        ("Firefox",             "firefox",        (89, 58, 177)),
-        ("LibreWolf",           "librewolf",      (0, 174, 255)),
+        ("Chrome",              "google-chrome-x64", (251, 191, 14)),
+        ("Edge",                "microsoft-edge",    (44, 195, 193)),
+        ("Brave (Recommended)", "brave",             (255, 47, 0)),
+        ("Firefox",             "firefox",           (89, 58, 177)),
+        ("LibreWolf",           "librewolf",         (0, 174, 255)),
     ]
     buttons = []
     for label, pkg_id, color in browser_options:
